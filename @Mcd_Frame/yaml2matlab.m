1789cbed8e1c89bbe5554353ef105830b12b616e
unknown <kgcmizes@gmail.com>
2013-06-19 15:13:58 -0400

    Mcd_Frame
    Fixed error in loop

--- @Mcd_Frame/yaml2matlab.m
+++ @Mcd_Frame/yaml2matlab.m
@@ -12,13 +12,14 @@ fid = fopen(file);
 if nargin==3;Mcd_Frame.seekToFirstFrame(fid, fSTART);end
 if nargin==1;Mcd_Frame.seekToFirstFrame(fid);end
 k=1;
-while(~feof(fid) || (nargin==3 && k==fEND-fSTART))
+while(~feof(fid))
 %for i = fSTART:fEND
     mcdf(k)=Mcd_Frame.readOneFrame(fid); %#ok<AGROW>
     k=k+1;
     if ~mod(k,100)
         disp(k);
     end
+    if nargin==3 && k==fEND-fSTART;break;end
 end
 if nargin==3;
 mcdf(1).FrameNumber = fSTART; %since skipped in finding start frame
